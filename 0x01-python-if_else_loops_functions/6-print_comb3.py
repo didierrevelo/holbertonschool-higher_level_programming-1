23893c8cf2b121069ead34a0f29c378148e8a3a1
didierrevelo <1829@holbertonschool.com>
2020-09-02 00:06:19 -0500

    print comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 10):
-    	for a in range(0, 10):
+        for a in range(0, 10):
                 if i < a:
                         print("{}{}".format(i, a), end="")
                         if i < 8:
