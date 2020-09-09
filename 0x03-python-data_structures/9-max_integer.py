25d5d7317e847f78339fa8a3bdfbb4baeec53ea5
didierrevelo <1829@holbertonschool.com>
2020-09-08 23:20:27 -0500

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
-        return None
-    mayor = my_list[0]
-    for j in my_list:
-        if j > mayor:
-            mayor = j
-    return (mayor)
+    if my_list:
+        mayor = my_list[0]
+        for j in my_list:
+            if j > mayor:
+                mayor = j
+        return (mayor)
