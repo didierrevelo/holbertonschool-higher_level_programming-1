a370b937f4c8b169be1dee32c862c788cc7ec797
didierrevelo <1829@holbertonschool.com>
2020-09-08 21:36:48 -0500

    correction4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    new_in_list(0, 0, 0)
-
-
 def new_in_list(my_list, idx, element):
-    if idx > 0 or idx < len(my_list) - 1:
+    if idx >= 0 and idx <= len(my_list) - 1:
         my_list = my_list.copy()
         my_list[idx] = element
     return (my_list)
