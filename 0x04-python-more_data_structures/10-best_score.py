467c512f236eb4c1df6ff93a0b18fbb07e089fe8
didierrevelo <1829@holbertonschool.com>
2020-09-10 22:20:26 -0500

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        resul = max(a_dictionary)
-        return resul
+        return max(a_dictionary, key = a_dictionary.get)
