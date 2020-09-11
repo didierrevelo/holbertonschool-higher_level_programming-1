3f83589490820f6fa1574545ef7d4dbdd2249c17
didierrevelo <1829@holbertonschool.com>
2020-09-10 19:10:46 -0500

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new = my_list[:]
-    if my_list:
-        for i, x in enumerate(new):
-            if x == search:
-                new[i] = replace
-        return new
+    for i, x in enumerate(new):
+        if x == search:
+            new[i] = replace
+    return new
