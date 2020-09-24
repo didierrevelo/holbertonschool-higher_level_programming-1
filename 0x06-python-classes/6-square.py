987e4ab876e7f92b98f116e59eedadc042d1a272
didierrevelo <1829@holbertonschool.com>
2020-09-23 23:25:54 -0500

    6 square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,7 +15,7 @@ class Square:
         """ area method
         return area square
         """
-        return self.__size ** 2
+        return self.__size * self.size
 
     @property
     def size(self):
@@ -40,8 +40,8 @@ class Square:
         if type(value) is not tuple or len(value) is not 2 or\
             type(value[0]) is not int or value[0] < 0 or\
                 type(value[1]) is not int or value[1] < 0:
-            raise TypeError("position must be a tuple\
-                        of 2 positive integers")
+            raise TypeError("position must be a tuple \
+                of 2 positive integers")
         else:
             self.__position = value
 
@@ -59,4 +59,3 @@ class Square:
                 for x in range(self.size):
                     print("#", end="")
                 print("")
-
