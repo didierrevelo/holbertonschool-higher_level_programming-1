63e1bd30fc414a8b6861648c2d5bf154ffbf3d0c
didierrevelo <1829@holbertonschool.com>
2020-09-23 22:13:25 -0500

    print square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" square """
+
+
+class Square:
+    """ class square empty"""
+    def __init__(self, size=0):
+        """ initialization of instance attributes
+        size (int): the size is an private instance attribute
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """ area method
+        return area square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """getter size """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """my_print method
+        print in stdout a square whit # symbol
+        or print "nothing" if size is equal to 0 """
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()
+        else:
+            print("")
