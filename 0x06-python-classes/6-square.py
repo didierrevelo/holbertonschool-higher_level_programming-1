45ddd9a19d373981caf97261a938b773be09e287
didierrevelo <1829@holbertonschool.com>
2020-09-23 23:17:06 -0500

    6 square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,12 +8,8 @@ class Square:
         """ initialization of instance attributes
         size (int): the size is an private instance attribute
         """
-        self.__size = size
-        self.__position = position
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
+        self.size = size
+        self.position = position
 
     def area(self):
         """ area method
@@ -36,14 +32,14 @@ class Square:
 
     @property
     def position(self):
-        """getter size """
+        """getter position """
         return self.__position
 
     @position.setter
     def position(self, value):
         if type(value) is not tuple or len(value) is not 2 or\
             type(value[0]) is not int or value[0] < 0 or\
-            type(value[1]) is not int or value[1] < 0:
+                type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple\
                         of 2 positive integers")
         else:
