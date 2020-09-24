878ad28e04154b23f6256073c3f207e63c76abfa
didierrevelo <1829@holbertonschool.com>
2020-09-23 19:49:18 -0500

    size instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" square """
+
+
+class Square:
+    def __init__(self, size):
+        """ initialization of instance attributes
+        size (int): the size is an private instance attribute
+        """
+        self.__size = size
