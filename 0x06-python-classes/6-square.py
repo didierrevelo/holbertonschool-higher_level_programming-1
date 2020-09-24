a977849969f2d99ded6961ed6c5daa8dd118002f
didierrevelo <1829@holbertonschool.com>
2020-09-23 23:10:43 -0500

    6 square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,6 +34,21 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        """getter size """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) is not 2 or\
+            type(value[0]) is not int or value[0] < 0 or\
+            type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple\
+                        of 2 positive integers")
+        else:
+            self.__position = value
+
     def my_print(self):
         """my_print method
         print in stdout a square whit # symbol
@@ -47,19 +62,4 @@ class Square:
                     print("#", end="")
                 print("")
         else:
-            print("")
-
-    @property
-    def position(self):
-        """getter size """
-        return self.__position
-
-    @position.setter
-    def position(self, value):
-        if type(value) is not tuple or len(value) is not 2 or\
-            type(value[0]) is not int or value[0] < 0 or\
-                type(value[1]) is not int or value[1] < 0:
-                    raise TypeError("position must be a tuple\
-                        of 2 positive integers")
-        else:
-            self.__position = value
+            print()
