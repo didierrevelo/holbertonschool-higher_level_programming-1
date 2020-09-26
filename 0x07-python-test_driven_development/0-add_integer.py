daab19930c3e9c0ad3763fc124d7cdaf776e865c
didierrevelo <1829@holbertonschool.com>
2020-09-26 00:40:17 -0500

    add method

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,9 +17,9 @@ def add_integer(a, b=98):
         return:
             result of add two integers
     """
-    if type(a) is not (int) and type(a) is not (float):
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type(b) is not (int) and type(b) is not (float):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
