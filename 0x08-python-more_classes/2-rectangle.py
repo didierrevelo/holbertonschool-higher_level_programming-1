75e744d900f3f1daa6660140d1f85defc3ea781c
didierrevelo <1829@holbertonschool.com>
2020-09-29 02:12:57 -0500

    area and perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -28,6 +28,8 @@ class Rectangle:
         Returns:
             perimeter: returns the add of sides
         """
+        if self.__width == 0 or self.__height == 0:
+                return 0
         return self.__width * 2 + self.__height * 2
 
     @property
