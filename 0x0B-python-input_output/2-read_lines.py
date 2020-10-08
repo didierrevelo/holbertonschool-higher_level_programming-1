86b9c5f518bcd1057e0286978477e8d2308393a8
didierrevelo <1829@holbertonschool.com>
2020-10-07 23:43:43 -0500

    2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""read file
+"""
 def read_lines(filename="", nb_lines=0):
+    """read line and print only lines
+    """
     count = 0
     with open(filename, encoding="utf-8") as f:
         for line in f:
