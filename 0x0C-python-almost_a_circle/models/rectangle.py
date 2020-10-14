452e03be5a1d9bf0dfd742454e7da24f906058b0
didierrevelo <1829@holbertonschool.com>
2020-10-14 17:55:56 -0500

    13 dict

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,4 +95,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """dict"""
-        return {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                 'x': self.x, 'y': self.y}
