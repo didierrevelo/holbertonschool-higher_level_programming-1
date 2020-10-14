d567f672e93180e1fc79b38ee839f7af67799be6
didierrevelo <1829@holbertonschool.com>
2020-10-13 23:15:43 -0500

    comment

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,10 +4,11 @@
 
 
 class Base:
-
+    """class base"""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """initializate method"""
         if id is None:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
