7526087458d29e52b1cf1b2486c8062d01821224
didierrevelo <1829@holbertonschool.com>
2020-10-14 18:47:07 -0500

    json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ class base for all proyect
 """
 import json
+from os import path
 
 
 class Base:
@@ -22,3 +23,22 @@ class Base:
         if not list_dictionaries:
             return('[]')
         return(json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+            Writes the string representation of an object of a class
+            into a file
+        """
+        filen = cls.__name__ + ".json"
+
+        list = []
+        if list_objs is not None:
+            for i in list_objs:
+                i = i.to_dictionary()
+                json_dict = json.loads(cls.to_json_string(i))
+                list.append(json_dict)
+
+        with open(filen, "w") as fd:
+            json.dump(list, fd)
+
