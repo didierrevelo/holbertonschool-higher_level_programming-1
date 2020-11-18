a382849f149efaf51926d82c71625b7d3329e047
didierrevelo <1829@holbertonschool.com>
2020-11-17 22:13:22 -0500

    14-average.sql

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,2 @@
+-- calcule el promedio de puntuación de todos los registros en la tabla second_tablede la base hbtn_0c_0de datos en su servidor MySQL.
+SELECT AVG(score) AS average FROM second_table;
