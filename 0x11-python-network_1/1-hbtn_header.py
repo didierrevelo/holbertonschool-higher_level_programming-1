9b10d07c81d14f80240e1ac45906d8c75f4f8909
didierrevelo <1829@holbertonschool.com>
2021-01-11 21:02:53 -0500

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
 """Response header value #0
 """
+import urllib.request
+import sys
+
 if __name__ == '__main__':
     url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
