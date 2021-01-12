02f98b9b4713e1d3134a2417d5be578fc267b414
didierrevelo <1829@holbertonschool.com>
2021-01-12 14:10:11 -0500

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-POST an email #1 
+POST an email #1
 """
 import requests
 import sys
 
-
-url = sys.argv[1]
-email = sys.argv[2]
-data = requests.post(url, {'email': email})
-print(data.text)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = requests.post(url, {'email': email})
+    print(data.text)
