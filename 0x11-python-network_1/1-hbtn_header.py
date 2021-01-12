603512c6ec9d61e5f0453884f528e2da5240f986
didierrevelo <1829@holbertonschool.com>
2021-01-11 21:01:16 -0500

    1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
-"""
-Write a Python script that takes in a URL,
-sends a request to the URL and displays the value
-of the X-Request-Id variable found in the header of the response.
+"""Response header value #0
 """
 if __name__ == '__main__':
     url = sys.argv[1]
