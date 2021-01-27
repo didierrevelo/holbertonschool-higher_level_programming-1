751ab77481bdc2f57381b8f13ff485f0cad9f107
didierrevelo <1829@holbertonschool.com>
2021-01-27 13:01:09 -0500

    5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+All cities by state
+"""
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    STATE = sys.argv[4]
+
+    conn = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER,
+                           passwd=MY_PASS, db=MY_DB, charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT GROUP_CONCAT(cities.name SEPARATOR ', ') \
+                FROM cities \
+                JOIN states \
+                ON cities.state_id=states.id \
+                WHERE cities.state_id=states.id \
+                AND states.name=%s \
+                ORDER BY cities.id ASC", (STATE,))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[0] is not None:
+            print(row[0])
+        else:
+            print()
+    cur.close()
+    conn.close()
