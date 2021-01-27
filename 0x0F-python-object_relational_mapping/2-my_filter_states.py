a4946da8fb96e74fe42a84f57678c3cad869f4ce
didierrevelo <1829@holbertonschool.com>
2021-01-27 12:30:06 -0500

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+    Task 2
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == '__main__':
+    MY_HOST = "localhost"
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    STATE = sys.argv[4]
+
+    conn = MySQLdb.connect(host=MY_HOST, port=3306, user=MY_USER,
+                           passwd=MY_PASS, db=MY_DB, charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name=\
+                '{}' ORDER BY id ASC".format(STATE))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
